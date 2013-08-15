f6a2c6db0a89cae1bee9b97514816bfcea126f43
Karl Ehlers <karl.ehlers@gmail.com>
2013-08-14 19:06:48 -0500

    Added blink function if USB cable is disconnected, cleaned up code.

--- MiscProjects/Speedometer DS/speedometer/speedometer.ino
+++ MiscProjects/Speedometer DS/speedometer/speedometer.ino
@@ -4,28 +4,39 @@ Servo drDre;
 int shooterSpeed = 0; // recieved value from laptop, 0-9
 int speedometerSpeed = 0; // value sent to Motor Controller 29
 int x = 0; // addition math variable
+int led = 6;
+
 void setup()
   {
     Serial.begin(9600); // Sets up communication with the serial monitor
     drDre.attach(9);
+    pinMode(led, OUTPUT);
   }
 
 void loop()
   {
-   if (Serial.available()>0) // Checks for a character in the serial monitor
-     {
-       int shooterSpeed = Serial.parseInt();
-         if (shooterSpeed = 0)
-           {
-             drDre.write(90);
-           }
-         else
-           {
-             int x = shooterSpeed * 2;
-             int speedometerSpeed = x + 110;
-             drDre.write(speedometerSpeed);
-           }
-     }    
+     if (Serial.available()>0) // Checks for a character in the serial monitor
+       {
+         digitalWrite(led, HIGH);
+         int shooterSpeed = Serial.parseInt();
+           if (shooterSpeed = 0)
+             {
+               drDre.write(90);
+             }
+           else
+             {
+               int x = shooterSpeed * 2;
+               int speedometerSpeed = x + 110;
+               drDre.write(speedometerSpeed);
+             }
+       }
+     else   // blink until signal is detected
+        {
+           digitalWrite(led, HIGH);
+           delay(1000);
+           digitalWrite(led, LOW);
+           delay(1000);
+        } 
 }
 
 
