85eddc7f2d4b9d1db0d6c2a426b7a3516cdcbd8f
Dylan Vener <dylanvener1@gmail.com>
2014-01-11 13:37:56 -0600

    added subsystem RMotor, and commands hold and three sec, for use with that subsystem

---
+++ 2014-AerialAssist/HolonomicPractice/Holonomic/holonomic/src/robot/commands/Hold.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.commands;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Hold extends CommandBase {
+    
+    public Hold() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ 2014-AerialAssist/HolonomicPractice/Holonomic/holonomic/src/robot/subsystems/RMotor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.subsystems;
+import robot.HW;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Dylan
+ */
+public class RMotor extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    Victor rmotor;
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public RMotor(){
+    rmotor = HW.
+    }
+}
