b15ab8b749f35566d0c05b4400965e201a980653
Patchosaur <patrickarevilla@gmail.com>
2014-01-16 18:10:41 -0600

    Mecanum Drive added to 2014 code, actually added drivetrain and commands that are necessary.

--- 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/CommandBase.java
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/CommandBase.java
@@ -3,7 +3,7 @@ package org.discobots.aerialassist.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.discobots.aerialassist.OI;
-import org.discobots.aerialassist.subsystems.ExampleSubsystem;
+import org.discobots.aerialassist.subsystems.Drivetrain;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
@@ -15,7 +15,7 @@ public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
-    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static Drivetrain drivetrain = new Drivetrain();
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
@@ -26,7 +26,7 @@ public abstract class CommandBase extends Command {
         oi = new OI();
 
         // Show what command your subsystem is running on the SmartDashboard
-        SmartDashboard.putData(exampleSubsystem);
+        SmartDashboard.putData(drivetrain);
     }
 
     public CommandBase(String name) {
---
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/MecanumDrive.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.discobots.aerialassist.commands;
+
+import com.sun.squawk.util.MathUtils;
+import org.discobots.aerialassist.GamePad;
+
+/**
+ *
+ * @author Patrick
+ */
+public class MecanumDrive extends CommandBase {
+    
+    GamePad J;
+    
+    public MecanumDrive(GamePad j) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(drivetrain);
+        J=j;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        drivetrain.holonomicPolar(0,0,0);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double x=J.getLX();
+        double y=J.getLY();
+        double rotation=J.getRX();
+        
+        double magnitude = Math.sqrt(x*x+y*y);
+        double angle = MathUtils.atan2(y,x);
+        
+        drivetrain.holonomicPolar(magnitude, angle, rotation);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        drivetrain.holonomicPolar(0,0,0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
