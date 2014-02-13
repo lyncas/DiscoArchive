8c2724b99430b3c6c56b867aa74bc7d46cd3b6c2
Nolan Shah <nolanshah212@gmail.com>
2014-02-12 23:30:19 -0600

    Deleted HolonomicPractice, Seth_Taylor, and IHatePaul folders in 2014 Added Pneumatapult and FirePneumatapult from Seth

---
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/upperbody/FirePneumatapult.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.discobots.aerialassist.commands.upperbody;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.discobots.aerialassist.commands.CommandBase;
+
+/**
+ *
+ * @author Seth
+ */
+public class FirePneumatapult extends CommandBase {
+    
+    private boolean shoot;
+    public static final boolean FIRE = true;
+    public static final boolean LOAD = false;
+    
+    public FirePneumatapult(boolean fire) {
+        requires(pneumatapultSub);
+        shoot = fire;
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+       SmartDashboard.putBoolean("Catapult status", shoot);
+       if (rollerSub.isExtended()) 
+        {
+            pneumatapultSub.fire(shoot);
+        } 
+       else
+       {
+           pneumatapultSub.fire(false);
+       }
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
---
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/subsystems/Pneumatapult.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.discobots.aerialassist.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Solenoid;
+import org.discobots.aerialassist.HW;
+
+/**
+ *
+ * @author Seth
+ */
+public class Pneumatapult extends Subsystem {
+    
+    private Solenoid fire1;
+    private Solenoid fire2;
+    
+    public void initDefaultCommand() {
+    }
+    
+    public Pneumatapult() {
+        fire1 = new Solenoid(HW.pneumatapultSolenoidA);
+        fire2 = new Solenoid(HW.pneumatapultSolenoidB);
+    }
+
+    public void fire(boolean shoot) {
+        fire1.set(shoot);
+        fire2.set(shoot);
+    }
+
+}
