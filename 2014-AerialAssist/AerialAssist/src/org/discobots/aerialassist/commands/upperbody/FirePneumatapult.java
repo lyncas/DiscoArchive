aca2468731d61197ecf276cc14e566a988a8b233
Dylan Vener <dylanvener1@gmail.com>
2014-02-15 16:17:23 -0600

    FirePneumatapult now supports three different modes of firing

--- 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/upperbody/FirePneumatapult.java
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/upperbody/FirePneumatapult.java
@@ -15,15 +15,21 @@ import org.discobots.aerialassist.commands.CommandBase;
 public class FirePneumatapult extends CommandBase {
     
     private boolean shoot;
+    private int check;
     public int count;
     public long startTime;
     private final long maxRunTime;
     public static final boolean FIRE = true;
     public static final boolean LOAD = false;
+    public static final int STUTTER = 0;
+    public static final int HALF = 1;
+    public static final int FULL = 2;
+
     
     
-    public FirePneumatapult(boolean fire) {
+    public FirePneumatapult(boolean fire, int ch) {
         requires(pneumatapultSub);
+        check = ch;
         shoot = fire;
         count=0;
         maxRunTime=1000;
@@ -36,14 +42,26 @@ public class FirePneumatapult extends CommandBase {
     protected void execute() {
        count+=20;
        SmartDashboard.putBoolean("Catapult status", shoot);
-       if (rollerSub.isExtended()) 
-        {
-                if(count%200<180)//200 is one fifth of the total execution time, the solenoid starts and stops 5 times now
-                    pneumatapultSub.fire(shoot);
-                else
-                    pneumatapultSub.fire(false);
-                if (!compressorSub.check())
-                {
+       if (rollerSub.isExtended()){      
+                if(check==0){
+                    if(count%200<180)//200 is one fifth of the total execution time, the solenoid starts and stops 5 times now
+                        pneumatapultSub.fire(shoot);
+                    else
+                        pneumatapultSub.fire(false);
+                }
+                if(check==1){
+                    if(count<=maxRunTime)
+                        pneumatapultSub.fire(shoot);
+                    else
+                        pneumatapultSub.fire(false);
+                }
+                if(check==2){
+                    if(count<=maxRunTime)
+                        pneumatapultSub.fire(shoot);
+                    else
+                        pneumatapultSub.fire(false);
+                }
+                if (!compressorSub.check()){
                     compressorSub.on();
                 }
         } 
@@ -62,5 +80,6 @@ public class FirePneumatapult extends CommandBase {
     }
 
     protected void interrupted() {
+        end();
     }
 }
