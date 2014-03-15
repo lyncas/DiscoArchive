695c8dde358ebcad2a113460108bd16cff20ac80
Nolan Shah <nolanshah212@gmail.com>
2014-03-15 12:05:46 -0500

    DALLAS COMMIT 4.5 (6) Adjusted mapping to reflect FirePneumatapult changes

--- 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/OI.java
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/OI.java
@@ -75,11 +75,16 @@ public class OI {
         b_btnB.whenPressed(new ToggleCompressor());
         b_sBack.whenPressed(new SetPneumaticsRunnable());
         b_clicR.whileHeld(new ResetGyroAngle());
-        b_dpadL.whenPressed(new SetMiniCimUsage());
+//        b_dpadL.whenPressed(new SetMiniCimUsage());
+        b_dpadU.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 0));
+        b_dpadL.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 1));
+        b_dpadD.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 3));
+        b_dpadR.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 4));
 // Controller 2
         b2_dpadU.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 0));
         b2_dpadL.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 1));
-        b2_dpadD.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 2));
+        b2_dpadD.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 3));
+        b2_dpadR.whenPressed(new FirePneumatapult(FirePneumatapult.FIRE, 4));
         b2_btnX.whenPressed(new SwitchDrive(SwitchDrive.MODE_OMNIWHEEL, SwitchDrive.MODE_NULL));
         b2_btnY.whenPressed(new SwitchDrive(SwitchDrive.MODE_TRACTION, SwitchDrive.MODE_NULL));
         b_bumpR.whenPressed(new ToggleArm(true));//arm up
