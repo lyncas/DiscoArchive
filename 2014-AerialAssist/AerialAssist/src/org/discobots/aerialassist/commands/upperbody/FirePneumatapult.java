a7baa8546b739ffb74950346b79afc87bb989997
Nolan Shah <nolanshah212@gmail.com>
2014-03-15 23:49:35 -0500

    DALLAS COMMIT 7 FirePneumatapult modified for testing length of shots

--- 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/upperbody/FirePneumatapult.java
+++ 2014-AerialAssist/AerialAssist/src/org/discobots/aerialassist/commands/upperbody/FirePneumatapult.java
@@ -13,7 +13,7 @@ import org.discobots.aerialassist.commands.CommandBase;
  * @author Seth
  */
 public class FirePneumatapult extends CommandBase {
-    
+
     private boolean shoot;
     private int check;
     public int count;
@@ -25,71 +25,71 @@ public class FirePneumatapult extends CommandBase {
     public static final int HALF = 1;
     public static final int FULL = 2;
 
-    
-    
     public FirePneumatapult(boolean fire, int ch) {
         requires(pneumatapultSub);
         check = pneumatapultSub.getMode();
         check = ch;
         shoot = fire;
-        count=0;
-        maxRunTime=1500;
-        switch (check){
+        count = 0;
+        maxRunTime = 1500;
+        switch (check) {
             case 0:
-                maxRunTime=500;
+                maxRunTime = 500;
                 break;
             case 1:
-                maxRunTime=1000;
+                maxRunTime = 1000;
                 break;
             case 2:
-                maxRunTime=1500;
+                maxRunTime = 1500;
                 break;
             case 3:
-                maxRunTime=1500;
+                maxRunTime = 1500;
                 break;
             case 4:
-                maxRunTime=250;
+                maxRunTime = (int) SmartDashboard.getNumber("FirePneumatapult Shot Time", 1500);
                 break;
         }
     }
 
     protected void initialize() {
-        startTime=System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
         System.out.println("Firing a shot of type " + check);
     }
 
     protected void execute() {
         if (rollerSub.isExtended()) {
-            switch (check){
-            case 0:
-                pneumatapultSub.fire(shoot);
-                break;
-            case 1:
-                pneumatapultSub.fire(shoot);
-                break;
-            case 2:
-                pneumatapultSub.fire(shoot);
-                break;
-            case 3:
-                if (count%80 != 0)
+            switch (check) {
+                case 0:
                     pneumatapultSub.fire(shoot);
-                else
-                    pneumatapultSub.fire(false);
-                break;
-            case 4:
-                pneumatapultSub.fire(shoot);
-                break;
-        }
+                    break;
+                case 1:
+                    pneumatapultSub.fire(shoot);
+                    break;
+                case 2:
+                    pneumatapultSub.fire(shoot);
+                    break;
+                case 3:
+                    if (count % 80 != 0) {
+                        pneumatapultSub.fire(shoot);
+                    } else {
+                        pneumatapultSub.fire(false);
+                    }
+                    break;
+                case 4:
+                    pneumatapultSub.fire(shoot);
+                    break;
+            }
             pneumatapultSub.fire(shoot);
-            count+=20;
+            count += 20;
         }
     }
 
     protected boolean isFinished() {
-        return System.currentTimeMillis()-startTime> maxRunTime;
+        return System.currentTimeMillis() - startTime > maxRunTime;
     }
 
     protected void end() {
+        System.out.println("Done firing in " + (System.currentTimeMillis() - startTime) + " milliseconds");
         pneumatapultSub.fire(false);
     }
 
